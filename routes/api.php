573ee6190ca5a20c67809bbcb7306b34fc3421a3
kengneruphine <ruphinekengne@gmail.com>
2021-12-11 04:13:34 +0100

    CRUD operation on products

---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+// Route::get('/products',[ProductController::class, 'index']);
+
+// Route::post('/products',[ProductController::class, 'store']);
+
+// Route::get('/products/{id}',[ProductController::class, 'show']);
+
+Route::resource('products', ProductController::class);
+
+Route::get('/products/search/{name}',[ProductController::class, 'search']);
+
+Route::group(['middleware' =>['auth:sanctum']],function(){
+
+});
+
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
